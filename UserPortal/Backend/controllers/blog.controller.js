87b96410ba134e9b5422c2df829eeb329d2c85c6
Himanshu <himanshukr.singh62@gmail.com>
2026-01-22 00:57:32 +0530

    Updated blog controller

--- UserPortal/Backend/controllers/blog.controller.js
+++ UserPortal/Backend/controllers/blog.controller.js
@@ -2,5 +2,5 @@ import asyncHandler from "express-async-handler";
 
 export const  deleteImage =asyncHandler(async(req,resizeBy,next)+>{
     const blogId= req.params.id;
-    const userId=Himanshu singh
+    const userId=Himanshu singh 2
 })
